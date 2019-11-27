ae9197ce9f0775d31ac5e2d15ee35d41c6d82fb9
gjasusa <g.jasusa@gmail.com>
2019-11-27 14:43:53 +0100

    correction

--- install_node.sh
+++ install_node.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
-sudo yum install -y gcc-c++ make -y nodejs -y
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
+yum install -y gcc-c++ make -y nodejs -y
 
-sudo npm install supervisor -g
+npm install supervisor -g
 
-sudo systemctl daemon-reload
-sudo systemctl enable --now nodejs
+systemctl daemon-reload
+systemctl enable --now nodejs
