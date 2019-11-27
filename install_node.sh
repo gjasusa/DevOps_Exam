ba85d200426aa046528518dfda9565bb755bac09
gjasusa <g.jasusa@gmail.com>
2019-11-27 14:56:40 +0100

    echo unit file for nodejs with supervisor

--- install_node.sh
+++ install_node.sh
@@ -5,5 +5,18 @@ yum install -y gcc-c++ make -y nodejs -y
 
 npm install supervisor -g
 
+echo "[Unit]
+Description=Node.JS Systemd Unit file with Supervisor listener
+Documentation=https://example.com
+After=network.target
+[Service]
+Environment=NODE_PORT=6111
+Type=simple
+User=root
+ExecStart=/bin/supervisor --watch /etc/nginx/ /etc/nginx/index.js
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target" > /lib/systemd/system/nodejs.service
+
 systemctl daemon-reload
 systemctl enable --now nodejs
